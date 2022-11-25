31a60a5d5ab7be440b6193dccec6b0f12ebd3ce7
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-11-25 13:28:43 +0100

    feat : add function to extract data from request

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ function convertToJson(request){
     return request.json();
 }
 
+function convertToObject(jsonRequest){
+    return {
+        name: jsonRequest.name,
+        feel: Math.floor(jsonRequest.main.feels_like + 0.5),
+        max : Math.floor(jsonRequest.main.temp_max + 0.5),
+        min : Math.floor(jsonRequest.main.temp_min + 0.5),
+        humi: jsonRequest.main.humidity,
+        desc: jsonRequest.weather[0].description,
+        icon: jsonRequest.weather[0].icon,
+        speed: Math.floor(jsonRequest.wind.speed + 0.5),
+        deg : jsonRequest.wind.deg
+    }
+}
+
+
 /**Lancer une requete pour une nouvelle Ville */
 function newCity(cityName){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`)
-    .then(request =>convertToJson(request))
-    .then(result => console.log(result))
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}&units=metric`)
+    .then(result =>convertToJson(result))
+    .then(result => {
+        let data = convertToObject(result);
+    })
 }
 
 newCity("Toulouse")
