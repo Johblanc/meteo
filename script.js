198c1795c992f009a53db07762e6217b71d22ece
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-11-25 13:50:47 +0100

    feat : add function to display data (just text elements yet)

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function convertToObject(jsonRequest){
         feel: Math.floor(jsonRequest.main.feels_like + 0.5),
         max : Math.floor(jsonRequest.main.temp_max + 0.5),
         min : Math.floor(jsonRequest.main.temp_min + 0.5),
-        humi: jsonRequest.main.humidity,
         desc: jsonRequest.weather[0].description,
         icon: jsonRequest.weather[0].icon,
+        humi: jsonRequest.main.humidity,
         speed: Math.floor(jsonRequest.wind.speed + 0.5),
         deg : jsonRequest.wind.deg
     }
@@ -26,14 +26,21 @@ function changeContentById(id, content){
 }
 
 
+/**Permet l'affichage des données récoltées */
+function refreshDisplay(data){
+    changeContentById("city-name", data.name);
+    changeContentById("temp-feel", data.feel);
+    changeContentById("temp-max", data.max);
+    changeContentById("temp-min", data.min);
+    changeContentById("humidity", data.humi);
+    changeContentById("speed", data.speed);
+}
+
 /**Lancer une requete pour une nouvelle Ville */
 function newCity(cityName){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}&units=metric`)
     .then(result =>convertToJson(result))
-    .then(result => {
-        let data = convertToObject(result);
-        console.log(data);
-    })
+    .then(result => refreshDisplay(convertToObject(result)))
 }
 
 newCity("Toulouse")
