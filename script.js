dee259b496fe7be912d4d982ea5ac283d563ff03
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-11-25 11:44:54 +0100

    feat : add function to GET API

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const APIKEY = "7a0ee5988d92e87091d7c7331b08e5be";
 
 /**Converti le resultat d'une requete en json */
 function convertToJson(request){
     return request.json();
 }
+
+/**Lancer une requete pour une nouvelle Ville */
+function newCity(cityName){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`)
+    .then(request =>convertToJson(request))
+    .then(result => console.log(result))
+}
+
+newCity("Toulouse")
